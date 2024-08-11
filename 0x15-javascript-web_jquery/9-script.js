efd120f8d8da5164349294150feab31ccc56667d
Rayane <rayanetoko@gmail.com>
2024-08-11 05:30:28 -0700

    pushing mandatory tasks

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,7 @@
+$(function() {
+    $.get('https://hellosalut.stefanbohacek.dev/?lang=fr', function(data, status) {
+        if (status === 'success') {
+            $('div#hello').append(data.hello);
+        }
+    })
+});
