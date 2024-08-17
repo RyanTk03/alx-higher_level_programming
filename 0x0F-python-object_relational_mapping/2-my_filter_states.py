e33b42246b543568be9071c3ade994767721f60d
Rayane <rayanetoko@gmail.com>
2024-08-17 05:47:32 -0700

    complete tasks for 0x0F-python-object_relational_mapping related to the use of mysqldb client: fix case insensitive error

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -18,8 +18,8 @@ if __name__ == '__main__':
 
     cursor = db.cursor()
 
-    cursor.execute('SELECT * FROM states WHERE name LIKE "{}" ORDER BY id ASC'
-                   .format(state_searched))
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY\
+                   id ASC".format(state_searched))
 
     for record in cursor.fetchall():
         print(record)
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -18,8 +18,8 @@ if __name__ == '__main__':
 
     cursor = db.cursor()
 
-    cursor.execute('SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC',
-                   (state_searched,))
+    cursor.execute('SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY\
+                   id ASC', (state_searched,))
 
     for record in cursor.fetchall():
         print(record)
