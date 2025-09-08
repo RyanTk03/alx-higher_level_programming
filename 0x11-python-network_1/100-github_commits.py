552cbc136cb4d30de752f2e3105289e6f8f0a159
ryntk <rayanetoko@proton.me>
2025-09-08 03:41:57 +0000

    chore(0x11-python-network_1): fix requests package tasks bugs

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,15 +6,15 @@ import requests
 
 
 if __name__ == "__main__":
-    url = "https://api.github.com/repos/{}/{}/commits".format(
+    url = "https://api.github.com/repos/{}/{}/commits?per_page=10".format(
         sys.argv[2], sys.argv[1])
 
     r = requests.get(url)
     commits = r.json()
     try:
-        for i in range(10):
+        for commit in commits:
             print("{}: {}".format(
-                commits[i].get("sha"),
-                commits[i].get("commit").get("author").get("name")))
+                commit.get("sha"),
+                commit.get("commit").get("author").get("name")))
     except IndexError:
         pass
